ede693e1a796727ff921f6475b6375f08f9a6a29
Michal J. Gajda <migamake@migamake.com>
2018-11-21 12:54:12 +0100

    Add GHC 8.6.2

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for GHC_VER in 7.8.4 7.10.3 8.0.2 8.2.2 8.4.4; do
+for GHC_VER in 7.8.4 7.10.3 8.0.2 8.2.2 8.4.4 8.6.2; do
   docker push migamake/haskell-build:${GHC_VER};
 done;
