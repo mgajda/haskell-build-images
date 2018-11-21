e8327d2415723dad76bb7cd7d3c9b7ad9013fda3
Michal J. Gajda <migamake@migamake.com>
2018-11-21 15:30:19 +0100

    Fixed build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 for GHC_VER in 7.8.4 7.10.3 8.0.2 8.2.2 8.4.4 8.6.2; do
-  docker build . -f Dockerfile --build-arg GHC_VER=${GHC_VER} --tag migamake/haskell-build:${GHC_VER} &&
+  docker build . -f Dockerfile --build-arg GHC_VER=${GHC_VER} --tag migamake/haskell-build:${GHC_VER} || exit 1;
 done;
