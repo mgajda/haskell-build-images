717de9a7a3b9abbb00a63233c535e0cca868113f
Michal J. Gajda <mjgajda@migamake.com>
2019-09-02 17:57:28 +0200

    Expand palette of GHC versions.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for GHC_VER in 7.8.4 7.10.3 8.0.2 8.2.2 8.4.4 8.6.2; do
-  docker build . -f Dockerfile --build-arg GHC_VER=${GHC_VER} --tag migamake/haskell-build:${GHC_VER} || exit 1;
+source config.sh
+
+for GHC_VER in "${GHC_VERSIONS[@]}"; do
+  docker build . -f Dockerfile --build-arg "GHC_VER=${GHC_VER}" --tag "migamake/haskell-build:${GHC_VER}" || exit 1;
 done;
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for GHC_VER in 7.8.4 7.10.3 8.0.2 8.2.2 8.4.4 8.6.2; do
-  docker push migamake/haskell-build:${GHC_VER};
+source ./config.sh
+
+for GHC_VER in "${GHC_VERSIONS[@]}"; do
+  docker push "migamake/haskell-build:${GHC_VER}";
 done;
